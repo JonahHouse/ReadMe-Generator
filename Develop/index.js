be03b63757403bef9d0e5237038b202855e798d8
Jonah Householder <jonahhouseholder@outlook.com>
2020-05-13 16:32:01 -0700

    Initial Commit

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// GIVEN the developer has a GitHub profile and a repository
+
+// WHEN prompted for the developer's GitHub username and repo specific information
+
+// THEN a README for the repo is generated
+
+
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
